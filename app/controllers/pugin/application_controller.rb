40c63180cfeafaf6eb34569c49e758c561192375
cwaszczuk <waszczukc@parliament.uk>
2016-11-22 11:02:02 +0000

    refactored method, added methods in helper (require / check), added alert partial, ammended panel partial to accept hash, removed shitty comment line in lib file.

--- app/controllers/pugin/application_controller.rb
+++ app/controllers/pugin/application_controller.rb
@@ -4,13 +4,8 @@ module Pugin
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    def self.get_component(component_name, model = nil)
-        if model.is_a?(Hash)
-            model = OpenStruct.new(model)
-            render :partial => "layouts/pugin/#{component_name}", locals: {object: model}
-        else
-            render :partial => "layouts/pugin/#{component_name}", locals: {object: model}
-        end
+    def self.get_component(component_name, properties = {})
+        render :partial => "layouts/pugin/#{component_name}", locals: {properties: properties}
     end
 
   end
--- app/helpers/pugin/application_helper.rb
+++ app/helpers/pugin/application_helper.rb
@@ -1,4 +1,13 @@
 module Pugin
   module ApplicationHelper
+    def render_if_exists(name, return_value = false)
+        name.nil? || name == "" ? false
+                                : return_value == true ? name : true
+    end
+
+    #def require_properties(*args)
+    #    args.any? {|arg| arg.nil?} ? {file_name: File.basename(__FILE__),}
+    #                               : true
+    #end
   end
 end
