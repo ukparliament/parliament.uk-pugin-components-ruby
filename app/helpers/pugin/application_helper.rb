4a0ada81b74a8584908cfebf89329488db72525d
cwaszczuk <waszczukc@parliament.uk>
2016-11-22 14:06:34 +0000

    removed code, fixed config file

--- app/helpers/pugin/application_helper.rb
+++ app/helpers/pugin/application_helper.rb
@@ -4,10 +4,5 @@ module Pugin
         name.nil? || name == "" ? false
                                 : return_value == true ? name : true
     end
-
-    #def require_properties(*args)
-    #    args.any? {|arg| arg.nil?} ? {file_name: File.basename(__FILE__),}
-    #                               : true
-    #end
   end
 end
