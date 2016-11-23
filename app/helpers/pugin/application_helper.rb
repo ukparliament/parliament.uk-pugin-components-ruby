c5702e44b0a9e712f2c0cc0b96053a8711720ed7
cwaszczuk <waszczukc@parliament.uk>
2016-11-23 16:44:37 +0000

    ammendments to helper, added contact file, updated panel and profile

--- app/helpers/pugin/application_helper.rb
+++ app/helpers/pugin/application_helper.rb
@@ -4,5 +4,9 @@ module Pugin
         name.nil? || name == "" ? false
                                 : return_value == true ? name : true
     end
+
+    def is_link?(description, link_address)
+      return link_address.nil? ? '<p>' + description + '</p>' : '<a href="' + link_address + '" target="_blank">' + description + "</a>"
+    end
   end
 end
