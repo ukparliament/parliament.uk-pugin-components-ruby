5934ed0864b0b97d05580db5eb3e45dd92bbd7f1
Jake Mulley <mulleyjb@parliament.uk>
2016-11-30 17:05:29 +0000

    feature(new-ap): update loader, watch task, browsersync

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,33 +1,57 @@
-const argv = require('minimist')(process.argv.slice(2)),
-      chokidar = require('chokidar'),
-      exec = require('child_process').exec,
-      log = console.log.bind(console),
-      watcher = chokidar.watch(argv._, { ignored: /[\/\\]\./ });
+// const argv     = require('minimist')(process.argv.slice(2)),
+//       chokidar = require('chokidar'),
+//       exec     = require('child_process').exec,
+//       log      = console.log.bind(console),
+//       pathn    = require('path'),
+//       pugin    = require('../../pugin.json');
 
-let task;
+// let keyCount = Object.keys(pugin).length;
+// let task;
+// let watcher;
+
+// var directoriesToWatch = [];
+// for(k in pugin) {
+//   keyCount = keyCount - 1;
+//   directoriesToWatch = directoriesToWatch.concat(pugin[k]);
+//   if(keyCount === 0) {
+//     initialise(directoriesToWatch);
+//   }
+// }
+
+// function initialise(directoriesToWatch) {
+//   watcher = chokidar.watch(directoriesToWatch, { ignored: /[\/\\]\./ });
+// }
+
+// function getTaskByDirectory(directory, path) {
+//   console.log(directory, path);
+// }
+
+// function test(path) {
+//   console.log(path);
+// }
 
 watcher
-  .on('ready', () => log(`Initialised watching for: ${argv._}`))
-  .on('change', recompile)
-  .on('unlink', recompile)
+  .on('ready', () => log('Watching files...'))
   .on('error', error => log(`Watcher error: ${error}`))
+  .on('addDir', test)
+  // .on('addDir', path => log(`Has been added: ${pathn.resolve(__dirname, `${path}`)}`))
+  .on('change', path => log(`Changed: ${path}`));
+  // .on('change', recompile)
+  // .on('unlink', recompile)
 
-function recompile(path) {
-  const fileExt = path.split('.').pop();
-  if(fileExt === 'scss') {
-    task = 'make css';
-  }
-  if(fileExt === 'js') {
-    task = 'make js';
-  }
-  if(task) {
-    exec(task, (error, stdout, stderr) => {
-      if(error === null && stdout) {
-        log(stdout);
-      } else {
-        log(`Task "${task}" failed:`);
-        log(error);
-      }
-    });
-  }
-}
+// // function recompile(path) {
+// //   const fileExt = path.split('.').pop();
+// //   if(fileExt === 'scss') {
+// //     task = 'make loader TASK=css';
+// //   }
+// //   if(task) {
+// //     exec(task, (error, stdout, stderr) => {
+// //       if(error === null && stdout) {
+// //         log(stdout);
+// //       } else {
+// //         log(`Task "${task}" failed:`);
+// //         log(error);
+// //       }
+// //     });
+// //   }
+// // }
