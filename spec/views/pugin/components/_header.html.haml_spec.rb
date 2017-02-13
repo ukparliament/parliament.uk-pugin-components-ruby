d229c56c600db49807b4b0748bdc3537a19660d4
cwaszczuk <waszczukc@parliament.uk>
2017-02-13 11:56:36 +0000

    set up config for environment variables, used in header

--- spec/views/pugin/components/_header.html.haml_spec.rb
+++ spec/views/pugin/components/_header.html.haml_spec.rb
@@ -13,7 +13,7 @@ describe 'pugin/components/_header.html.haml', type: :view do
 <div class='row'>
 <div class='header__logo'>
 <a href='/'>
-<img alt='parliament.uk' src='http://ukpds.pugin-website.s3-website-eu-west-1.amazonaws.com/images/parliament-uk--white-50.png' srcset='http://ukpds.pugin-website.s3-website-eu-west-1.amazonaws.com/images/parliament-uk--white-50.svg'>
+<img alt='parliament.uk' src='#{ENV["ASSET_LOCATION_URL"]}/images/parliament-uk--white-50.png' srcset='#{ENV["ASSET_LOCATION_URL"]}/images/parliament-uk--white-50.svg'>
 </a>
 </div>
 </div>
