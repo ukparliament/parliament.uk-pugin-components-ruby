824803941a1cf9362d14a61b223c3c5cdf05314c
Matt Rayner <mattrayner@users.noreply.github.com>
2017-02-16 15:59:34 +0000

    Update status partial to use correct links

--- spec/views/pugin/components/_status.html.haml_spec.rb
+++ spec/views/pugin/components/_status.html.haml_spec.rb
@@ -12,7 +12,7 @@ describe 'pugin/components/_status.html.haml', type: :view do
 <div class='c-fs status'>
 <div class='c-fg'>
 <div class='row'>
-<p>This is a <a href="beta-services.html"><strong>BETA</strong></a>. Give <a href="feedback.html">feedback</a> to improve it.</p>
+<p>This is a <a href="/"><strong>BETA</strong></a>. Give <a href="#">feedback</a> to improve it.</p>
 </div>
 </div>
 </div>
@@ -30,7 +30,7 @@ DATA
 <div class='c-fs status__test'>
 <div class='c-fg'>
 <div class='row'>
-<p>This is a <a href="beta-services.html"><strong>BETA</strong></a>. Give <a href="feedback.html">feedback</a> to improve it.</p>
+<p>This is a <a href="/"><strong>BETA</strong></a>. Give <a href="#">feedback</a> to improve it.</p>
 </div>
 </div>
 </div>
