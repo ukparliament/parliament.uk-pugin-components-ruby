988419bfc373e34955b8f5581b0712c5fc6745c2
Peter Hunt <naseberry@users.noreply.github.com>
2017-02-23 15:17:33 +0000

    [minor-update] changed unicode ampersand to ampersand [minor-update] update test

--- spec/views/pugin/components/_status.html.haml_spec.rb
+++ spec/views/pugin/components/_status.html.haml_spec.rb
@@ -10,8 +10,8 @@ describe 'pugin/components/_status.html.haml', type: :view do
       expect(response).to eq(
 <<DATA
 <div class='highlight__status'>
-<div class='c-fs status'>
-<div class='c-fg'>
+<div class='container--full-screen status'>
+<div class='container--full-grid'>
 <div class='row--indent-horizontal'>
 <p>This is a <a href="/"><strong>BETA</strong></a>. Give <a target="_blank" title="website opens in a new window" href="https://ukparliament-self.achieveservice.com/en/AchieveForms/?form_uri=sandbox-publish://AF-Process-032e1b1d-958f-4402-9ab2-97a38ed9b899/AF-Stage9b7ecae8-0c85-4ee7-afda-b256cffe5f08/definition.json&amp;redirectlink=%2F&amp;cancelRedirectLink=%2F&amp;category=AF-Category-48936b5c-9329-48c7-a0a5-563463aacadf">feedback</a> to improve it.</p>
 </div>
@@ -30,8 +30,8 @@ DATA
       expect(rendered).to eq(
 <<DATA
 <div class='highlight__status'>
-<div class='c-fs status__test'>
-<div class='c-fg'>
+<div class='container--full-screen status__test'>
+<div class='container--full-grid'>
 <div class='row--indent-horizontal'>
 <p>This is a <a href="/"><strong>BETA</strong></a>. Give <a target="_blank" title="website opens in a new window" href="https://ukparliament-self.achieveservice.com/en/AchieveForms/?form_uri=sandbox-publish://AF-Process-032e1b1d-958f-4402-9ab2-97a38ed9b899/AF-Stage9b7ecae8-0c85-4ee7-afda-b256cffe5f08/definition.json&amp;redirectlink=%2F&amp;cancelRedirectLink=%2F&amp;category=AF-Category-48936b5c-9329-48c7-a0a5-563463aacadf">feedback</a> to improve it.</p>
 </div>
