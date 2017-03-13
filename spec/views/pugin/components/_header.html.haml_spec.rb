dd693abb90c89a905a3cd28a471df75355cff3d0
Usman Afzal <afzalu@parliament.uk>
2017-03-13 18:06:22 +0000

    [WEBSITE-751] update to header/footer markup and tests

--- spec/views/pugin/components/_header.html.haml_spec.rb
+++ spec/views/pugin/components/_header.html.haml_spec.rb
@@ -8,12 +8,12 @@ describe 'pugin/components/_header.html.haml', type: :view do
 
     expect(response).to eq(<<DATA
 <header>
-<div class='c-fs header'>
-<div class='c-fg'>
+<div class='container--full-screen header'>
+<div class='container--full-grid'>
 <div class='row--indent-horizontal'>
 <div class='header__logo'>
 <a href='/'>
-<img alt='parliament.uk' src='#{Rails.application.secrets.asset_location_url}/images/parliament-uk--white.png' srcset='#{Rails.application.secrets.asset_location_url}/images/parliament-uk--white.svg'>
+<img alt='parliament.uk' src='//images/parliament-uk--white.png' srcset='//images/parliament-uk--white.svg'>
 </a>
 </div>
 </div>
