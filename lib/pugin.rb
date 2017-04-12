b8d9d165b1dae7ca7291d8abfb9cbb4b4b31c246
Usman Afzal <afzalu@parliament.uk>
2017-04-12 15:38:49 +0100

    [Release] Version bump up for pugin and pugin-components

--- lib/pugin.rb
+++ lib/pugin.rb
@@ -3,6 +3,6 @@ require 'pugin/railtie'
 require 'pugin/version'
 
 module Pugin
-  ASSET_VERSION = '1.2.7'.freeze
+  ASSET_VERSION = '1.2.8'.freeze
   ASSET_LOCATION_URL = ENV.fetch('ASSET_LOCATION_URL', 'https://s3-eu-west-1.amazonaws.com/web1devci.pugin-website')
 end
