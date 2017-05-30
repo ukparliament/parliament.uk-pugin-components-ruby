d706c4bb2eb147a0c1575c5639164769e7915772
Peter Hunt <naseberry@users.noreply.github.com>
2017-05-30 17:19:29 +0100

    hotfix: main navigation (#189)

--- spec/views/pugin/layouts/pugin.html.haml_spec.rb
+++ spec/views/pugin/layouts/pugin.html.haml_spec.rb
@@ -6,9 +6,11 @@ describe 'pugin/layouts/pugin.html.haml', type: :view do
     let(:person) { double('Grom::Node') }
 
     before :each do
+      HousesHelper = Class.new
       allow(person).to receive(:name).and_return('Jane Smith')
       allow(person).to receive(:position).and_return('Minister for Tests')
       allow(person).to receive(:id).and_return('123')
+      allow(HousesHelper).to receive(:commons_id).and_return('123')
     end
 
     it 'renders HAML without errors' do
