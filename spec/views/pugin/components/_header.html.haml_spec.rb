a706d87889a2cdc3da41f1c4d047a72956c2ad66
cwaszczuk <waszczukc@parliament.uk>
2017-06-15 16:55:48 +0100

    [WEBSITE-1451] Asset and structural changes

--- spec/views/pugin/components/_header.html.haml_spec.rb
+++ spec/views/pugin/components/_header.html.haml_spec.rb
@@ -10,7 +10,7 @@ describe 'pugin/components/_header.html.haml', type: :view do
 <header>
 <div class='container'>
 <a href='/'>
-<img alt='UK Parliament' class='logo' src='https://s3-eu-west-1.amazonaws.com/web1live.pugin-website/1.4.6/images/uk_parliament_logo.png' srcset='https://s3-eu-west-1.amazonaws.com/web1live.pugin-website/1.4.6/images/uk_parliament_logo.svg'>
+<img alt='UK Parliament' class='logo' src='https://s3-eu-west-1.amazonaws.com/web1live.pugin-website/images/uk_parliament_logo_white.png' srcset='https://s3-eu-west-1.amazonaws.com/web1live.pugin-website/images/uk_parliament_logo_white.svg'>
 </a>
 </div>
 </header>
