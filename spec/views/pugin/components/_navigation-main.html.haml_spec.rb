d2a64959c053ec98856b24ee49a716e64b6a932b
Usman Afzal <usman@usmanafzal.co.uk>
2017-08-25 16:24:56 +0100

    [fix] constituency link change to navigation--main (#235)

--- spec/views/pugin/components/_navigation-main.html.haml_spec.rb
+++ spec/views/pugin/components/_navigation-main.html.haml_spec.rb
@@ -17,7 +17,7 @@ describe 'pugin/components/_navigation-main.html.haml', type: :view do
   	allow(Parliament::Utils::Helpers::HousesHelper).to receive(:lords_id).and_return('456')
 		allow(view).to receive(:mps_path).and_return("/mps")
 		allow(view).to receive(:house_members_current_a_z_letter_path).and_return("houses/#{Parliament::Utils::Helpers::HousesHelper.lords_id}/members/current/a-z/a")
-		allow(view).to receive(:constituencies_current_a_z_letter_path).and_return("constituencies/current/a-z/a")
+		allow(view).to receive(:find_your_constituency_path).and_return("/find-your-constituency")
 		allow(view).to receive(:house_parties_current_path).and_return("houses/#{Parliament::Utils::Helpers::HousesHelper.commons_id}/parties/current/")
 	end
 
@@ -26,7 +26,7 @@ describe 'pugin/components/_navigation-main.html.haml', type: :view do
 		expect(rendered).to include("<nav aria-label='navigation' class='navigation--main'>")
 		expect(rendered).to include('<a href="/mps">MPs</a>')
 		expect(rendered).to include('<a href="houses/456/members/current/a-z/a">Lords</a>')
-		expect(rendered).to include('<a href="constituencies/current/a-z/a">Constituencies</a>')
+		expect(rendered).to include('<a href="/find-your-constituency">Constituencies</a>')
 		expect(rendered).to include('<a href="houses/123/parties/current/">Parties and groups</a>')
 	end
 end
