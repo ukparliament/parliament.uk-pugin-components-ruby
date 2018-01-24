8d730ba5e61dc0a1d953470a9efd905239c85229
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-01-24 16:45:15 +0000

    Changes to people/list views following changes to Parliaments
    * Renamed _member and _lord partial test files as per RSpec specifications
    * Removed _list partial test files as tests are covered elsewhere
    * Amended tests following refactoring of partial views
    * Defined logic for a parliament Lord now that Lords can be linked to a Parliament
    * Refactored _member, _lord and _list people/list partials
    * Changed logic on people/_list partial to ensure that the correct partial is applied for each person

--- spec/lib/pugin/helpers/controller_helpers_spec.rb
+++ spec/lib/pugin/helpers/controller_helpers_spec.rb
@@ -1,42 +1,43 @@
 require 'spec_helper'
 require 'pugin/helpers/controller_helpers'
 
-describe 'pugin/helpers/controller_helpers.rb' do 
+describe 'pugin/helpers/controller_helpers.rb' do
 
-	before do 
+	before do
 		class TestClass
 			include Pugin::Helpers::ControllerHelpers
-		end 
+		end
 	end
 
 	subject { TestClass.new }
 
 	context '#enable_status_banner' do
-		it 'enables the status banner' do 
+		it 'enables the status banner' do
 			subject.enable_status_banner
 			expect(Pugin::Feature::StatusBanner.enabled?).to equal(true)
 		end
 	end
-	context '#disable_status_banner' do 
 
-		before :each do 
+	context '#disable_status_banner' do
+		before :each do
 			Pugin::Feature::StatusBanner.enable
 		end
 
-		it 'disables the status banner' do 
+		it 'disables the status banner' do
 			expect(Pugin::Feature::StatusBanner.disabled?).to equal(false)
 			subject.disable_status_banner
 			expect(Pugin::Feature::StatusBanner.disabled?).to equal(true)
 		end
 	end
+
 	context '#enable_global_search' do
 		it 'enables the global search' do
 			subject.enable_global_search
 			expect(Pugin::Feature::GlobalSearch.enabled?).to equal(true)
 		end
 	end
-	context '#disable_global_search' do
 
+	context '#disable_global_search' do
 		before :each do
 			Pugin::Feature::GlobalSearch.enable
 		end
@@ -47,25 +48,27 @@ describe 'pugin/helpers/controller_helpers.rb' do
 			expect(Pugin::Feature::GlobalSearch.disabled?).to equal(true)
 		end
 	end
-	context '#enable_top_navigation' do 
-		it 'enables the top navigation' do 
+
+	context '#enable_top_navigation' do
+		it 'enables the top navigation' do
 			subject.enable_top_navigation
 			expect(Pugin::Feature::TopNavigation.enabled?).to equal(true)
 		end
 	end
-	context '#disable_top_navigation' do 
 
-		before :each do 
+	context '#disable_top_navigation' do
+		before :each do
 			Pugin::Feature::TopNavigation.enable
 		end
 
-		it 'disables the top navigation' do 
+		it 'disables the top navigation' do
 			expect(Pugin::Feature::TopNavigation.disabled?).to equal(false)
 			subject.disable_top_navigation
 			expect(Pugin::Feature::TopNavigation.disabled?).to equal(true)
 		end
 	end
-	context '#reset_bandiera_features' do 
+	
+	context '#reset_bandiera_features' do
 		before :each do
 			Pugin::Feature::Bandiera.instance_variable_set(:@features, {'show-register' => true, 'show-dissolution' => true})
 		end
