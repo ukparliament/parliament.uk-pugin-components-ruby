f08f9b5057b681ef384ef09d16570dc1c842c8e2
Usman Afzal <usman@usmanafzal.co.uk>
2018-01-29 14:21:29 +0000

    [RELEASE] Bump of components and pugin (#277)

--- lib/pugin.rb
+++ lib/pugin.rb
@@ -1,7 +1,7 @@
 require 'pugin/version'
 
 module Pugin
-  ASSET_VERSION = '1.7.2'.freeze
+  ASSET_VERSION = '1.7.3'.freeze
   ASSET_LOCATION_URL = ENV.fetch('ASSET_LOCATION_URL', 'https://s3-eu-west-1.amazonaws.com/web1live.pugin-website')
   STATIC_ASSET_LOCATION_URL = ENV.fetch('STATIC_ASSET_LOCATION_URL', 'https://s3-eu-west-1.amazonaws.com/web1live.static-assets')
   class << self
