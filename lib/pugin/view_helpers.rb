e4bae5123e649ce5ebd1d71fd4df6c268bba26bb
Jake Mulley <mulleyjb@parliament.uk>
2018-02-12 15:05:43 +0000

    Implement basis for OpenGraph tagging

--- lib/pugin/view_helpers.rb
+++ lib/pugin/view_helpers.rb
@@ -1,10 +1,25 @@
 module Pugin
-    module ViewHelpers
-        def navigation_link(route_symbol:, route_args:, letter:)
-            url = send(route_symbol, *route_args, letter) unless route_args.empty?
-            url = send(route_symbol, letter) unless url
-
-            return link_to(letter, url, data: { 'atoz-see': 'true' })
-        end
+  module ViewHelpers
+    def navigation_link(route_symbol:, route_args:, letter:)
+      url = send(route_symbol, *route_args, letter) unless route_args.empty?
+      url = send(route_symbol, letter) unless url
+      return link_to(letter, url, data: { 'atoz-see': 'true' })
     end
+
+    def opengraph_image_standards
+      { width: '400', height: '400', twitter_card: 'summary', url: "#{STATIC_ASSET_PUBLIC_LOCATION_URL}/opengraph-oblong.png" }
+    end
+
+    def opengraph_image
+      image_hash = if @image
+                     {url: "#{ENV['IMAGE_SERVICE_URL']}/#{@image.graph_id}.jpeg?crop=CU_1:1&width=400&quality=100"}
+                   elsif @person && !@image
+                     {url: "#{ENV['IMAGE_SERVICE_URL']}/#{@person.image_id}.jpeg?crop=CU_1:1&width=400&quality=100"}
+                   else
+                     {width: '1200', height: '630', twitter_card: 'summary_large_image' }
+                   end
+
+      opengraph_image_standards.merge(image_hash)
+    end
+  end
 end
