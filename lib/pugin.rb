d17989a3203cdc8fb0aceb1ac5ff1732dae3f8e7
Usman Afzal <afzalu@parliament.uk>
2018-05-16 12:48:55 +0100

    Updated the asset urls to use subdomain

--- lib/pugin.rb
+++ lib/pugin.rb
@@ -2,9 +2,9 @@ require 'pugin/version'
 
 module Pugin
   ASSET_VERSION = '1.9.1'.freeze
-  ASSET_LOCATION_URL = ENV.fetch('ASSET_LOCATION_URL', 'https://s3-eu-west-1.amazonaws.com/web1live.pugin-website')
+  ASSET_LOCATION_URL = ENV.fetch('ASSET_LOCATION_URL', 'https://static.parliament.uk/pugin')
   STATIC_ASSET_LOCATION_URL = ENV.fetch('STATIC_ASSET_LOCATION_URL', 'https://s3-eu-west-1.amazonaws.com/web1live.static-assets')
-  STATIC_ASSET_PUBLIC_LOCATION_URL = ENV.fetch('STATIC_ASSET_PUBLIC_LOCATION_URL', 'https://s3-eu-west-1.amazonaws.com/web1live.static-assets-public')
+  STATIC_ASSET_PUBLIC_LOCATION_URL = ENV.fetch('STATIC_ASSET_PUBLIC_LOCATION_URL', 'https://static.parliament.uk/assets-public')
   class << self
 		attr_writer :alternates
 
