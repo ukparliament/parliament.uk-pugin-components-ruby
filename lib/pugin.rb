83bc1e418705b3ea5af337b70d618d494c8b2584
Joshua Waheed <joshua.waheed@gmail.com>
2018-06-20 13:18:15 +0100

    [RELEASE] Pugin version bump (#320)

--- lib/pugin.rb
+++ lib/pugin.rb
@@ -1,7 +1,7 @@
 require 'pugin/version'
 
 module Pugin
-  ASSET_VERSION = '1.9.4'.freeze
+  ASSET_VERSION = '1.10.0'.freeze
   ASSET_LOCATION_URL = ENV.fetch('ASSET_LOCATION_URL', 'https://static.parliament.uk/pugin')
   STATIC_ASSET_LOCATION_URL = ENV.fetch('STATIC_ASSET_LOCATION_URL', 'https://s3-eu-west-1.amazonaws.com/web1live.static-assets')
   STATIC_ASSET_PUBLIC_LOCATION_URL = ENV.fetch('STATIC_ASSET_PUBLIC_LOCATION_URL', 'https://static.parliament.uk/assets-public')
